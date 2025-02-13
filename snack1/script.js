a226716694e0bb07522be0ee2cf12248e03e5f74
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-13 10:41:49 +0100

    add if number are equal

--- snack1/script.js
+++ snack1/script.js
@@ -1,13 +1,16 @@
 /*L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.*/
 
-const userChoiceFirst = Number (prompt("Scrivi un numero [es. 4, 10, 40 ...]"));
-const userChoiceSecond = Number (prompt("Scrivi un altro numero diverso dal primo [es. 5, 11, 41 ...]"));
+const userChoiceFirst = Number(prompt("Scrivi un numero [es. 4, 10, 40 ...]"));
+const userChoiceSecond = Number(prompt("Scrivi un altro numero diverso dal primo [es. 5, 11, 41 ...]"));
 
-const greaterNumber = userChoiceFirst > userChoiceSecond ;
+const greaterNumber = userChoiceFirst > userChoiceSecond;
+const similarNumber = userChoiceFirst === userChoiceSecond;
 
 if (greaterNumber) {
-   console.log(userChoiceFirst)
+    console.log(userChoiceFirst)
+} else if (similarNumber) {
+    console.log("i due numeri sono uguali")
 } else {
     console.log(userChoiceSecond)
 }
