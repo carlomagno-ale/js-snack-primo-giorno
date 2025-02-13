66aa945a98a086f946f4faffc17ddbe2d80c38a6
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-13 12:44:08 +0100

    add es snack4

---
+++ snack4/script.js
@@ -0,0 +1,15 @@
+/*Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.*/
+
+const oddNumbers = [];
+
+for (let i = 0; i < 6; i++) {
+    const userNumber = Number(prompt('inserisci un numero'));
+
+    if (userNumber % 2 === 1) {
+        oddNumbers.push(userNumber);
+    } 
+}
+
+console.log(oddNumbers)
