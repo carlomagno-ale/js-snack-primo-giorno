8e88912af906860ab3695c0f77eb56fe6076e9f1
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-13 12:29:45 +0100

    add sum

--- snack3/script.js
+++ snack3/script.js
@@ -1,13 +1,11 @@
 /*Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.*/
 
-const sum = [];
+let sum = 0;
 
 for (let i = 0; i < 10; i++) {
     const thisNumber = Number(prompt('inserisci un numero'));
-    //somma
-
+    sum = sum + thisNumber;
 }
 
 console.log(sum)
-
